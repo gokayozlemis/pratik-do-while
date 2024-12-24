f6c32cdaca460ae74d612c9736eb4ec4ed35e754
gokay <gokay@GOKAY>
2024-12-24 19:23:21 +0300

    Add project files.

---
+++ pratik while do-while/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Bir limit değeri girin: ");
+        int limit = int.Parse(Console.ReadLine());
+
+        int sayac = 0;
+
+        do
+        {
+            Console.WriteLine("Ben bir Patika'lıyım");
+            sayac++;  
+        } while (sayac <= limit);
+    }
+}
+
+
+
+
+
+
+
+
+
